fb7f4f5c162cf5b2932168e61cd85a5cb0e205f9
Mickaël Canévet <mickael.canevet@camptocamp.com>
2017-05-19 13:05:38 +0200

    Use bundled pem file

--- fetch-rancher-certificate.sh
+++ fetch-rancher-certificate.sh
@@ -22,13 +22,14 @@ fi
 mkdir -p "$CERT_DIR"
 
 TEMP_DIR=$(mktemp -d -p /tmp)
-get_cert_val "$CERT_NAME" "key" > "${TEMP_DIR}/${CERT_NAME}.key"
-get_cert_val "$CERT_NAME" "cert" > "${TEMP_DIR}/${CERT_NAME}.crt"
+get_cert_val "$CERT_NAME" "key" > "${TEMP_DIR}/${CERT_NAME}.pem"
+get_cert_val "$CERT_NAME" "cert" >> "${TEMP_DIR}/${CERT_NAME}.pem"
 get_cert_val "$CERT_NAME" "certchain" > "${TEMP_DIR}/${CERT_NAME}.certchain"
 
-if [[ $(< "${TEMP_DIR}/${CERT_NAME}.certchain") = "null" ]]; then
-  rm "${TEMP_DIR}/${CERT_NAME}.certchain"
+if [[ $(< "${TEMP_DIR}/${CERT_NAME}.certchain") != "null" ]]; then
+  cat "${TEMP_DIR}/${CERT_NAME}.certchain" >> "${TEMP_DIR}/${CERT_NAME}.pem"
 fi
+rm "${TEMP_DIR}/${CERT_NAME}.certchain"
 
 if diff -N -q -r "$CERT_DIR" "$TEMP_DIR" > /dev/null; then
   rm -rf "$TEMP_DIR"
