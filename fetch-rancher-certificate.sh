84a8b40f047cb185d974e33c04e1bf26fe33058b
Raphaël Pinson <raphael.pinson@camptocamp.com>
2017-06-01 15:37:02 +0200

    Support retrieving all certificates

--- fetch-rancher-certificate.sh
+++ fetch-rancher-certificate.sh
@@ -4,37 +4,50 @@ CATTLE_CONFIG_URL_V2=${CATTLE_CONFIG_URL/v1/v2-beta}
 CERT_NAME="$1"
 CERT_DIR=${2:-/usr/local/etc/haproxy/certs}
 
+ALL_CERTS_JSON=$(curl -s  -H "Authorization: ${CATTLE_AGENT_INSTANCE_AUTH}" "${CATTLE_CONFIG_URL_V2}/certificates")
+TEMP_DIR=$(mktemp -d -p /tmp)
+
 function get_cert_val() {
   local name="$1" k="$2"
-  json=$(curl -s  -H "Authorization: ${CATTLE_AGENT_INSTANCE_AUTH}" "${CATTLE_CONFIG_URL_V2}/certificates")
   if test $? -ne 0; then
     echo "Can't fetch ${name} certificate." >&2
     exit 1
   fi
-  jq -r ".data[] | select(.name == \"${name}\").${k}" <<<"${json}"
+  jq -r ".data[] | select(.name == \"${name}\").${k}" <<<"${ALL_CERTS_JSON}"
 }
 
-if test -z "$CERT_NAME"; then
-  echo "Please specify certname" >&2
-  exit 1
-fi
+function get_cert() {
+  local name="$1"
+
+  get_cert_val "$name" "key" > "${TEMP_DIR}/${name}.pem"
+  get_cert_val "$name" "cert" >> "${TEMP_DIR}/${name}.pem"
+  get_cert_val "$name" "certchain" > "${TEMP_DIR}/${name}.certchain"
+
+  if [[ $(< "${TEMP_DIR}/${name}.certchain") != "null" ]]; then
+    cat "${TEMP_DIR}/${name}.certchain" >> "${TEMP_DIR}/${name}.pem"
+  fi
+  rm "${TEMP_DIR}/${name}.certchain"
+}
+
+function get_all_certs() {
+  jq -r ".data[].name" <<<"${ALL_CERTS_JSON}" | while read c; do
+    get_cert "$c"
+  done
+}
 
 mkdir -p "$CERT_DIR"
 
-TEMP_DIR=$(mktemp -d -p /tmp)
-get_cert_val "$CERT_NAME" "key" > "${TEMP_DIR}/${CERT_NAME}.pem"
-get_cert_val "$CERT_NAME" "cert" >> "${TEMP_DIR}/${CERT_NAME}.pem"
-get_cert_val "$CERT_NAME" "certchain" > "${TEMP_DIR}/${CERT_NAME}.certchain"
-
-if [[ $(< "${TEMP_DIR}/${CERT_NAME}.certchain") != "null" ]]; then
-  cat "${TEMP_DIR}/${CERT_NAME}.certchain" >> "${TEMP_DIR}/${CERT_NAME}.pem"
+if test -z "$CERT_NAME" -o "$CERT_NAME" = "ALL" ; then
+  get_all_certs
+else
+  get_cert "$CERT_NAME"
 fi
-rm "${TEMP_DIR}/${CERT_NAME}.certchain"
 
 if diff -N -q -r "$CERT_DIR" "$TEMP_DIR" > /dev/null; then
   rm -rf "$TEMP_DIR"
 else
-  cp "$TEMP_DIR"/* "$CERT_DIR"
+  rm -f "$CERT_DIR"/*
+  mv "$TEMP_DIR"/* "$CERT_DIR"
   rm -rf "$TEMP_DIR"
 
   pkill -HUP -f haproxy-systemd-wrapper
